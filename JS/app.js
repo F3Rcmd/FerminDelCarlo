2149bb626cabedc44f2946bfef570cb32a4db380
F3R <fernuevo12345@gmail.com>
2022-08-28 21:33:36 -0300

    Simulador Interactivo

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,31 @@
-let numero = Number(prompt("Ingresa un numero"));
-
-for (let x = 0; x <= 20; x++){
-
-  let resultado = numero * x;
-
-  alert(`${numero} x ${x} = ${resultado}`);
+function nombre() {
+  let nombrecomp = prompt("ingresa tu nombre de usuario");
+  while(nombrecomp.length <= 5) {
+    alert("es un nombre muy corto");
+    nombrecomp = prompt("ingresa tu nombre de usuario");
+  }
+  if (nombrecomp.length >= 5) {email();}
 }
+
+function email() {
+  let mail = prompt("ingresa tu email");
+  password();
+}
+
+function password() {
+  let pass = prompt("ingresa una contraseña");
+  let check = prompt("confirma tu contraseña"); 
+  while(check != `${pass}`){
+    alert("la contraseña no coincide");
+    pass = prompt("ingresa una contraseña");
+    check = prompt("confirma tu contraseña");
+  }
+  if (check == `${pass}`) {finish();}
+}
+
+function finish() {
+  alert("registro terminado");
+}
+
+
+nombre();
