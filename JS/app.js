295b2ed34f229ad5e561ac591d749159c1eb1373
F3R <fernuevo12345@gmail.com>
2022-09-05 13:14:55 -0300

    Array agregada

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,30 @@
+const cuenta =  [];
+
 function nombre() {
   let nombrecomp = prompt("ingresa tu nombre de usuario");
+  cuenta.push({ id: "nombre", nombrecomp});
   while(nombrecomp.length <= 5) {
     alert("es un nombre muy corto");
     nombrecomp = prompt("ingresa tu nombre de usuario");
+    cuenta.push({ id: "nombre", nombrecomp});
   }
   if (nombrecomp.length >= 5) {email();}
 }
 
 function email() {
   let mail = prompt("ingresa tu email");
+  cuenta.push({ id: "mail", mail});
   password();
 }
 
 function password() {
   let pass = prompt("ingresa una contraseña");
+  cuenta.push({ id: "contraseña", pass});
   let check = prompt("confirma tu contraseña"); 
   while(check != `${pass}`){
     alert("la contraseña no coincide");
     pass = prompt("ingresa una contraseña");
+    cuenta.push({ id: "contraseña", pass});
     check = prompt("confirma tu contraseña");
   }
   if (check == `${pass}`) {finish();}
@@ -25,7 +32,10 @@ function password() {
 
 function finish() {
   alert("registro terminado");
+  console.log(cuenta)
 }
 
 
 nombre();
+
+
