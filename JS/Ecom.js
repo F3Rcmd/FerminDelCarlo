11116f5c4bd082e4b3e93cd2ee781cd06511bb84
F3R <fernuevo12345@gmail.com>
2022-10-18 20:30:46 -0300

    Proyecto final

--- JS/Ecom.js
+++ JS/Ecom.js
@@ -16,24 +16,114 @@ document.getElementById("titulo").textContent = "Bienvenido " + nombre;
 
 /* toma los datos del storage y los envia al array cuenta */
 
-function alerta() {
-  Swal.fire('Any fool can use a computer')  
+let carro = [];
+
+let precioFinal = [];
+
+function prod1() {
+  const item1 = { id: 1, nombre: "Producto 1", precio: 1000 }
+
+  carro.push(item1);
 }
 
-const muestra = document.getElementById("api")
+function prod2() {
+  const item2 = { id: 2, nombre: "Producto 2", precio: 2000 }
 
-const apinfo = async()=>{
-    const recibe = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const devuelve = await recibe.json()
-
-    devuelve.forEach((post)=>{
-        const li = document.createElement("li")
-        li.innerHTML=`
-        <h4>${post.id}</h4>
-        <h5>${post.title}</h5>
-        `
-        muestra.append(li)
-    })
+  carro.push(item2);
 }
 
-apinfo();
+function prod3() {
+  const item3 = { id: 3, nombre: "Producto 3", precio: 3000 }
+
+  carro.push(item3);
+}
+
+function Dprod1() {
+  carro = carro.filter(prod => prod.precio !== 1000);
+}
+
+function Dprod2() {
+  carro = carro.filter(prod => prod.precio !== 2000);
+}
+
+function Dprod3() {
+  carro = carro.filter(prod => prod.precio !== 3000);
+}
+
+const deseados = document.getElementById("Deseados");
+
+const mostrarF = document.getElementById("mostrarfinal");
+
+function show() {
+  for (producto of carro) {
+    let lista = document.createElement("div");
+    lista.innerHTML = `
+    <h4>${producto.nombre}</h4>
+    <b>${producto.precio}$</b>
+    `;
+    deseados.appendChild(lista);
+  }
+
+  const total = carro.map(prod => prod.precio).reduce((ant, act) => ant + act,);
+
+  precioFinal.push(total);
+
+  mostrarF.innerHTML = `
+  <h5>Total:</h5>
+  <b>${total}$</b>`
+}
+
+function erase() {
+  deseados.innerHTML = " ";
+  
+  mostrarF.innerHTML = " ";
+}
+
+function Dall() {
+  carro = [];
+
+  deseados.innerHTML = " ";
+  
+  mostrarF.innerHTML = " ";
+}
+
+function comprar() {
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger'
+  },
+  buttonsStyling: false
+})
+
+swalWithBootstrapButtons.fire({
+  title: '¿Comprar?',
+  text: "Se te enviara un mail para continuar",
+  icon: 'question',
+  showCancelButton: true,
+  confirmButtonText: 'Si!',
+  cancelButtonText: 'No...',
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+    swalWithBootstrapButtons.fire(
+      'De acuerdo!',
+      'Verifica si tu mail para continuar.',
+      'success'
+    )
+  } else if (
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire(
+      'Cancelada',
+      'La compra no sea realizado',
+      'error'
+    )
+  }
+})
+};
+
+
+
+
+
